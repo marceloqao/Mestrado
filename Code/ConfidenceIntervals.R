80654d17e93e7fbbb2e58752ed9454969ff16406
acfrery <acfrery@gmail.com>
2017-10-16 11:48:41 -0300

    Por favor revisa os quantis

--- Code/ConfidenceIntervals.R
+++ Code/ConfidenceIntervals.R
@@ -1,7 +1,31 @@
-require(taRifx)
+attach(HC)
 
-summary(HC)
+dEuclid.quantiles = data.frame()
 
-q <- c(1, 10, 50, 100, 1000) / 1000
+q <- c(1/1000, 10/1000, 50/1000, 100/1000)
 
-sort(HC, f = dEuclid)
+for(s in 1:length(levels(Source))) {
+  for(n in 1:length(levels(N))) {
+    for(d in 1:length(levels(D))) {
+      for(t in 1:length(levels(tau))) {
+        HC.current <- subset(HC, 
+                                  Source==levels(Source)[s] & 
+                                  N==levels(N)[n] & 
+                                  D==levels(D)[d] & 
+                                  tau==levels(tau)[t]
+                                  )
+        dEuclid.range <- range(HC.current$dEuclid)
+        print(dEuclid.range)
+        dEuclid.quantiles <- rbind(dEuclid.quantiles, 
+                               c(dEuclid.range[1], 
+                                 quantile(HC.current$dEuclid, q, type=4),
+                                 dEuclid.range[2],
+                                 levels(Source)[s], levels(N)[n], levels(D)[d], levels(tau)[t]
+                               )
+        )
+      }  
+    }
+  }
+}
+
+names(dEuclid.quantiles) <- c("min", "1/1000", "10/1000", "50/1000", "100/1000", "max", "Source", "N", "D", "tau")
