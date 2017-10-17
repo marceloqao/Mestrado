d1a09823077b9c98d51af077a8f39916935ff370
acfrery <acfrery@gmail.com>
2017-10-17 06:15:38 -0300

    Fazer os gráficos de quantis

--- Code/ConfidenceIntervals.R
+++ Code/ConfidenceIntervals.R
@@ -1,30 +1,8 @@
-attach(HC)
+require(Hmisc)
 
-dEuclid.quantiles = data.frame()
+quant = function(x){quantile(x, probs=c(1/1000, 10/1000, 50/1000, 100/1000), type=4)}
+quantis.por.fator <- summarize(HC$dEuclid, llist(Source, N, D, tau), quant)
+names(quantis.por.fator) <- c( "Source", "N", "D", "tau", "0.1%", "1%", "5%", "10%")
 
-q <- c(1/1000, 10/1000, 50/1000, 100/1000)
+quantis.por.fator
 
-
-for(s in 1:length(levels(Source))) {
-  for(n in 1:length(levels(N))) {
-    for(d in 1:length(levels(D))) {
-      for(t in 1:length(levels(tau))) {
-        dEuclid.current <- subset(HC$dEuclid, 
-                                  Source==levels(Source)[s] & 
-                                  N==levels(N)[n] & 
-                                  D==levels(D)[d] & 
-                                  tau==levels(tau)[t]
-                                  )
-        dEuclid.range <- range(dEuclid.current)
-        print(dEuclid.range)
-        dEuclid.quantiles <- rbind(dEuclid.quantiles, 
-                                        c(dEuclid.range[1], quantile(dEuclid.current, q, type=4), dEuclid.range[2],
-                                 levels(Source)[s], levels(N)[n], levels(D)[d], levels(tau)[t]
-                               )
-        )
-      }  
-    }
-  }
-}
-
-names(dEuclid.quantiles) <- c("min", "1/1000", "10/1000", "50/1000", "100/1000", "max", "Source", "N", "D", "tau")
