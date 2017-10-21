b5368b5adf4530120a414bba2016e9bb02c2ea89
marceloqao <marceloqao@gmail.com>
2017-10-21 18:43:57 -0300

    Some new graphics and organizing files of text.

--- Code/HIstograms.R
+++ Code/HIstograms.R
@@ -1,10 +1,10 @@
 # Histogramas das distâncias
 
 # Todos os histogramas de distâncias
-ggplot(data=subset(HC, D=="6" & N=="1000"), aes(x=dEuclid)) +
+ggplot(data=subset(HC, D=="6" & N=="50k" & tau=="50"), aes(x=dEuclid)) +
   geom_density(aes(fill=Source), alpha=.5) +
   scale_x_continuous() +
-  facet_grid(~tau) +
+  #facet_grid(~tau) +
   #facet_wrap(~tau) +
   theme_light()
 
