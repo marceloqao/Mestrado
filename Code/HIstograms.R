e04b37226a959506d9f0d08f6ab22a34155965f0
marceloqao <marceloqao@gmail.com>
2017-10-21 11:24:17 -0300

    Adicionados todos os histogramas e todas as tabelas dos testes de Kolmogorov-Smirnov

--- Code/HIstograms.R
+++ Code/HIstograms.R
@@ -1,17 +1,14 @@
 # Histogramas das distâncias
 
-
 # Todos os histogramas de distâncias
-ggplot(data=subset(HC, N=="50k" & D==6 & tau==50), aes(x=dEuclid)) +
+ggplot(data=subset(HC, D=="6" & N=="1000"), aes(x=dEuclid)) +
   geom_density(aes(fill=Source), alpha=.5) +
   scale_x_continuous() +
-  #facet_wrap(D~ tau) +
+  facet_grid(~tau) +
+  #facet_wrap(~tau) +
   theme_light()
-ks.test(x=subset(HC, N=="50k" & D==6 & tau==1 & Source=="Quantum")$dEuclid,
-        y=subset(HC, N=="50k" & D==6 & tau==1 & Source=="Radio")$dEuclid)
 
 # Visualização de histogramas por grupos
-
 
 ggplot(data=subset(data, D==4), aes(x=value)) +
   geom_density(aes(fill=tau), alpha=.5) +
