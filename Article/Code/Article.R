8f9f276cb223be17b51f142688f37ddd97308674
acfrery <acfrery@gmail.com>
2018-08-30 10:08:31 -0300

    avançando

--- Article/Code/Article.R
+++ Article/Code/Article.R
@@ -4,8 +4,6 @@ summary(results$X)
 values <- data.frame(TN=as.factor(results$TN),
                      K=as.factor(results$K),
                      D=as.factor(results$D),
-                     Rep=as.factor(results$REP),
-                     Tau=as.factor(results$TAU),
                      H=results$H,
                      C=results$C,
                      JS=results$JS)
