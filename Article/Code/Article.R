93948d04b7aff7a44ff44c5cccc48438c4f04358
Alejandro C. Frery <acfrery@gmail.com>
2019-02-02 12:39:08 -0300

    Avançando

--- Article/Code/Article.R
+++ Article/Code/Article.R
@@ -1,12 +1,15 @@
-results <- read.csv("~/Documents/Alunos/Marcelo Queiroz/Article/Data/results.csv")
-summary(results$X)
+results <- read.table("../Data/result.txt", head=TRUE)
+summary(results)
 
-values <- data.frame(TN=as.factor(results$TN),
-                     K=as.factor(results$K),
-                     D=as.factor(results$D),
-                     H=results$H,
-                     C=results$C,
-                     JS=results$JS)
+results <- results[,-4]
+results$D <- as.factor(results$D)
 
-rm(results)
+require(ggplot2)
+require(ggthemes)
 
+df3 <- subset(results, D == 3)[-3]
+df4 <- subset(results, D == 4)[-3]
+df5 <- subset(results, D == 5)[-3]
+df6 <- subset(results, D == 6)[-3]
+
+ggplot(subset(df3, TN=1000), aes(x=H, y=C)) + geom_point(aes(color=K), alpha=0.1)
