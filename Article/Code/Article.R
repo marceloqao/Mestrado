82b420e6c0de0f8fffd2a50121f2e0763b450741
Alejandro C. Frery <acfrery@gmail.com>
2019-02-09 11:35:40 -0300

    Por favor verificar

--- Article/Code/Article.R
+++ Article/Code/Article.R
@@ -1,6 +1,9 @@
 results <- read.table("../Data/result.txt", head=TRUE)
 
-results <- results[,-4]
+summary(results)
+results <- results[,-c(4,5)]
+summary(results)
+
 results$D <- as.factor(results$D)
 results$TN <- as.factor(results$TN)
 results$K <- as.factor(results$K)
@@ -14,26 +17,27 @@ require(ggfortify)
 ### Exploratory analysis
 
 ggplot(
-  subset(results, 
-         TN==levels(results$TN[1]) & 
-         K==levels(results$K)[6] & 
-         D==levels(results$D)[1])) +
-  geom_point(aes(x=H, y=C))
-
-### Ver, a partir de este gráfico, quais fatores podem ser considerados relevantes
-### Analisar TN=1000, D=6 e ver fazer regressões de C ~ H com K como fator; o fator é relevante?
-
-ggplot(
   results,
   aes(x=H, y=C, col=D)
 ) + geom_point() + facet_grid(K ~ TN) 
 
 ### ESTRANHO!!!
 ggplot(
-  subset(results, TN==1000),
+  subset(results, TN==5e5),
   aes(x=H, y=C, col=D)
 ) + geom_point() + facet_grid(~ K) 
 
+### Verificando se há algum problema
+
+## Seleciono um tamanho de vetor, uma dimensão e dois K
+subset12 <- subset(results, TN==1000 & D==3 & (K==0 | K==2))
+
+## Desenho os valores selecionados
+ggplot(subset12, aes(x=H, y=C)) + geom_point() + facet_grid(~K)
+
+### NÃO VEJO DIFERENÇA!!!
+
+### NÃO LER A PARTIR DESTE PONTO
 ### PCA
 autoplot(prcomp(df_D3_TN1000_K0[, c(4,5)]),
          loadings = TRUE, loadings.colour = 'blue',
